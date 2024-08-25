d9b91d3730c6d29d1f6f6cffb89ec80c459da4ab
Mamun Rashid <mamun001@gmail.com>
2024-08-25 08:02:02 -0700

    map funcstion

---
+++ 033_use_map_function/main.py
@@ -0,0 +1,16 @@
+
+# BUILT-IN MAP FUNCTION
+
+my_list = ["jo","jil","pete","pauly"]
+
+def add_space (s):
+    return s + " "
+    
+
+mapped_list = list(map(add_space,my_list))
+
+print(mapped_list)
+
+
+
+
