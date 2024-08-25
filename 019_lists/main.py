a8e02cdb779dad7cd76d23443e4832dca7f9099d
Mamun Rashid <mamun001@gmail.com>
2024-08-24 17:45:23 -0700

    added lists

---
+++ 019_lists/main.py
@@ -0,0 +1,19 @@
+
+
+players = ["Magnus", "Hikaru", "Nepo"]
+players.append("John")
+print(players)
+
+n = players.count("Nepo")
+print(n)
+
+players.remove("John")
+print(players)
+
+last_player=players.pop()
+print(last_player)
+print(players)
+
+
+
+
