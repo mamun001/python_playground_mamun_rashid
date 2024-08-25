98a619a1b1b1952e71f02dd33324faccf41c3e3d
Mamun Rashid <mamun001@gmail.com>
2024-08-25 15:20:46 -0700

    args and kwargs

---
+++ 040_args_and_kwargs_into_function/main.py
@@ -0,0 +1,15 @@
+
+# Use of *args and **kwargs
+
+def print_args_and_kwargs (*args, **kwargs) :
+    print(args)
+    print(kwargs)
+
+
+
+print_args_and_kwargs("GM",10,20.5,name="Magnus",rating="2800")
+
+
+
+
+
