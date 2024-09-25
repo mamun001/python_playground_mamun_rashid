d212cf7fde09b5e24fb4243c1c4bd33e12e9fa1c
Mamun Rashid <mamun001@gmail.com>
2024-09-24 21:12:43 -0700

    pass ; do nothing function and classes

---
+++ 060_use_pass_to_create_do_nothing_functions_and_classes/main.py
@@ -0,0 +1,15 @@
+
+# When you run this code, there will no errors
+
+# make a function that does nothing
+def function1():
+   pass
+
+# make a class that has nothing, does nothing
+class class1:
+   pass
+
+# call the function
+function1()
+# make an object of the class1 type
+foo = class1()
