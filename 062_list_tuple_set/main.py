05f72a31ce888fb99bc1c61ab78c630fde4a6462
Mamun Rashid <mamun001@gmail.com>
2024-09-25 16:18:31 -0700

    062 list tuple set

---
+++ 062_list_tuple_set/main.py
@@ -0,0 +1,19 @@
+
+# List , Tuple and Set
+
+# List
+L = ['a','b','c']
+print(type(L))
+L.append('d')
+print(L)
+
+#Tuple, immutable
+T = ('a','b')
+print(type(T))
+print(len(T))
+
+#Set
+S = {'a','b'}
+print(type(S))
+S.add('c')
+print(len(S))
